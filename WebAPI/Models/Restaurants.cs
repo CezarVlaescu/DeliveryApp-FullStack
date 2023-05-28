5fa242b874aef0f1c510830b0398154e21d38d41
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2023-05-28 08:53:41 +0300

    Add files via upload

---
+++ WebAPI/Models/Restaurants.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodDeliveryAppAPI.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int openingTime { get; set; }
+        public int closedTime { get; set; }
+        public int maxDeliveryDistance { get; set; }
+        public int maxFeeDelivery { get; set;}
+        public bool extraFee { get; set;}
+
+    }
+}
