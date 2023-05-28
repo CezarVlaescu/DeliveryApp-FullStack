8353a815eaeda10286a563ec32d08bf826b0b984
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2023-05-28 08:54:19 +0300

    Add files via upload

---
+++ WebAPI/Controllers/RestaurantsController.cs
@@ -0,0 +1,36 @@
+﻿using FoodDeliveryAppAPI.Context;
+using FoodDeliveryAppAPI.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDeliveryAppAPI.Controllers
+{
+    public class RestaurantsController : ControllerBase
+    {
+        private readonly AppDBContext _dbContext;
+        public RestaurantsController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [EnableCors("MyPolicy")]
+        [HttpPost("AddRestaurant")]
+
+        public async Task<IActionResult> AddRestaurant([FromBody] Restaurant restObj)
+        {
+            if(restObj == null)
+            {
+                return BadRequest();
+            }
+            var restaurant = await _dbContext.Restaurants.FirstOrDefaultAsync(y => y.Id == restObj.Id && y.Name == restObj.Name);
+            if (restaurant == null)
+            {
+                return NotFound(new { Message = "No restaurant" });
+            }
+            return Ok(new
+            {
+                Message = "Restaurant added succesfully"
+            });
+        }
+    }
+}
