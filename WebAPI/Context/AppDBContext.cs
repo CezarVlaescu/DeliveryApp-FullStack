9f7286e125e337afaedf159ee43a937730fb3ee3
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2023-05-28 08:55:17 +0300

    Add files via upload

---
+++ WebAPI/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using FoodDeliveryAppAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDeliveryAppAPI.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options) 
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Restaurant>().ToTable("restaurant");
+        }
+    }
+}
